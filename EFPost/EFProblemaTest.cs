888f7865a3d4f11f3962c12f1652acdbd58e554d
Leonardo Silvio de Moura <leonardosimoura@gmail.com>
2017-06-16 08:13:21 -0300

    commit

---
+++ EFPost/EFProblemaTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EFPost
+{
+    public class EFSolucaoTest
+    {
+        [Fact]
+        public void ProblemaSalvarMultiplasVezes()
+        {
+            var usuario = new Usuario()
+            {
+                UsuarioId = Guid.NewGuid(),
+                Nome = "Leonardo"
+            };
+
+            using (var context = new ProblemaContext())
+            {
+                context.Usuarios.Add(usuario);
+                context.SaveChanges();              
+            }
+
+            using (var context = new ProblemaContext())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var usuarioTest = new Usuario
+                    {
+                        UsuarioId = usuario.UsuarioId,
+                        Nome = "alterado"
+                    };
+
+                    context.Usuarios.Attach(usuarioTest);
+                    context.Entry(usuarioTest).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                Assert.Equal(context.Usuarios.FirstOrDefault().Nome, "alterado");
+                Assert.Equal(context.Usuarios.FirstOrDefault().UsuarioId, usuario.UsuarioId);
+            }
+        }        
+    }
+}
--- EFPost/EFSolucaoTest.cs
+++ EFPost/EFSolucaoTest.cs
@@ -8,42 +8,8 @@ using Xunit;
 
 namespace EFPost
 {
-    public class EFTests
-    {
-        [Fact]
-        public void ProblemaSalvarMultiplasVezes()
-        {
-            var usuario = new Usuario()
-            {
-                UsuarioId = Guid.NewGuid(),
-                Nome = "Leonardo"
-            };
-
-            using (var context = new ProblemaContext())
-            {
-                context.Usuarios.Add(usuario);
-                context.SaveChanges();              
-            }
-
-            using (var context = new ProblemaContext())
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    var usuarioTest = new Usuario
-                    {
-                        UsuarioId = usuario.UsuarioId,
-                        Nome = "alterado"
-                    };
-
-                    context.Usuarios.Attach(usuarioTest);
-                    context.Entry(usuarioTest).State = EntityState.Modified;
-                    context.SaveChanges();
-                }
-                Assert.Equal(context.Usuarios.FirstOrDefault().Nome, "alterado");
-                Assert.Equal(context.Usuarios.FirstOrDefault().UsuarioId, usuario.UsuarioId);
-            }
-        }
-
+    public class EFSolucaoTest
+    {       
         [Fact]
         public void SolucaoSalvarMultiplasVezes()
         {
